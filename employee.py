a1056614af58e50c8b66e182a5cf78083aa44526
Sage Hogue <Sagehogue@gmail.com>
2018-04-06 10:38:07 -0700

    removing some comments

--- employee.py
+++ employee.py
@@ -1,10 +1,3 @@
-# Driver list presumably already returned from method donald is building.
-# Same as above in regards to order list.
-
-
-# so I need to tell donald I would like an available_driver_list and current_order_list
-# tell donald about my plans for a dispatch_dict
-
 
 class Employee:
     def __init__(self, name, emp_id, wage_code=0):
@@ -13,8 +6,6 @@ class Employee:
         self.wage = wage_dict[wage_code]
 
     def dispatch(self, driver_id, order_id):
-        # signify in some way that the driver is now busy - change attribute if class or something
-        # maybe return key:value pair of driver and current order?
         driver_list.remove(driver_id)
         dispatch_pair = {driver_id: order_id}
         dispatch_dict.update(dispatch_pair)
@@ -32,12 +23,13 @@ def gen_id():
     id_list.append(new_id)
     return new_id
 
+
 wage_dict = {0: 11.25, 1: 12.50, 2: 15.50, 3: 14.75, 4: 13.25}
 if __name__ == '__main__':
     id_list = [0, 1]
-    order_list = []  # this should be retur
-    driver_list = []  # this should be returned to me from donald's method
-    wage_dict = {0: 11.25, 1: 12.50, 2: 15.50, 3: 14.75, 4: 13.25, 5: 25.25}  # not sure where to naturally integrate
+    order_list = []
+    driver_list = []
+    wage_dict = {0: 11.25, 1: 12.50, 2: 15.50, 3: 14.75, 4: 13.25, 5: 25.25}
     dispatch_dict = {}
     TestEmp = Employee('Sullivan', gen_id())
     TestEmp.setwage(1)
